7192a1421b996dff040a58ad4743b5bd74ddbd5e
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-01-03 12:06:15 +0100

    updating latex installation
    - restored full install
    - updated bibtex keys

--- osx_install_tex_live.sh
+++ osx_install_tex_live.sh
@@ -20,8 +20,8 @@ brew upgrade
 # Install native apps
 brew tap caskroom/cask
 brew install brew-cask
-# brew cask install mactex
-brew cask install basictex
+brew cask install mactex
+# brew cask install basictex
 brew cask install bibdesk
 brew cask install skim
 brew cask install texshop
@@ -39,11 +39,11 @@ sudo chown -R $USER  /usr/local/texlive
 tlmgr update --self
 tlmgr update --all
 
-tlmgr install adjustbox
-tlmgr install collectbox
-tlmgr install ucs
-tlmgr install collection-fontsrecommended
-tlmgr install collection-latexrecommended
+# tlmgr install adjustbox
+# tlmgr install collectbox
+# tlmgr install ucs
+# tlmgr install collection-fontsrecommended
+# tlmgr install collection-latexrecommended
 
 # setting some preferences in TexShop
 #defaults write TeXShop NSUserKeyEquivalents -dict-add "Typeset" "@\U21a9"
@@ -52,11 +52,11 @@ defaults write TeXShop "BibTeXengine" -string "biber"
 defaults write TeXShop "Encoding"  -string  "UTF-8"
 
 # setting some preferences in BibDesk (see http://invibe.net/LaurentPerrinet/SciBlog/2013-03-06):
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Key Format" -string "%p1%y%u0"
-defaults write edu.ucsd.cs.mmccrack.bibdesk BDSKLocalFileFormatKey -string "%f{Cite Key}%n0%e"
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Path to the papers folder" -string "Biblio"
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite String" -string "citep"
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Key Autogenerate" -int 1
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Key Format Preset" -int 0
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Cite Prepend Tilde" -int 1
-defaults write edu.ucsd.cs.mmccrack.bibdesk "Startup Behavior" -int 4
+defaults write bibdesk "Cite Key Format" -string "%p1%y%u0"
+defaults write bibdesk BDSKLocalFileFormatKey -string "%f{Cite Key}%n0%e"
+defaults write bibdesk "Path to the papers folder" -string "Biblio"
+defaults write bibdesk "Cite String" -string "citep"
+defaults write bibdesk "Cite Key Autogenerate" -int 1
+defaults write bibdesk "Cite Key Format Preset" -int 0
+defaults write bibdesk "Cite Prepend Tilde" -int 1
+defaults write bibdesk "Startup Behavior" -int 4
