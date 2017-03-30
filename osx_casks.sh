c2e24e2fa9dcfdf7e290e62e7fbd213783264eac
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-03-30 08:57:33 +0200

    Adding clementine 1.3.1

--- osx_casks.sh
+++ osx_casks.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env zsh
-brew install homebrew/completions/brew-cask-completion
+brew install brew-cask-completion
 # Install native apps
 brew tap caskroom/cask
 # brew install brew-cask
@@ -8,10 +8,10 @@ brew tap caskroom/versions
 
 brew cask uninstall --force evernote macvim openoffice inkscape filezilla cakebrew  todotxt tunnelblick  tomahawk  adobe-reader clamxav  dropbox thunderbird  unison pineapple arduino zotero
 
-export APPS=( jupyter-notebook-ql atom docker-toolbox libreoffice virtualbox kitematic github-desktop owncloud  psychopy wireshark java flash-player firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package sourcetree meld)
+export APPS=( jupyter-notebook-ql atom docker-toolbox libreoffice virtualbox kitematic github-desktop owncloud  psychopy wireshark java flash-player firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package sourcetree meld clementine)
 for i in $APPS
 do
   echo '📸  Installing ' $i
-  brew cask uninstall --force $i
+  # brew cask uninstall --force $i
   brew cask install $i
 done
