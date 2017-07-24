549afa83e067d559a97d6b0a4f8d02662437f36c
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-07-24 16:03:00 +0200

    fix

--- osx_casks.sh
+++ osx_casks.sh
@@ -6,9 +6,9 @@ brew tap caskroom/cask
 brew tap caskroom/fonts
 brew tap caskroom/versions
 
-brew cask uninstall --force evernote macvim openoffice inkscape filezilla cakebrew  todotxt tunnelblick  tomahawk  adobe-reader clamxav  dropbox thunderbird  unison pineapple arduino zotero flash-player
+brew cask uninstall --force evernote macvim openoffice inkscape filezilla cakebrew  todotxt tunnelblick  tomahawk  adobe-reader clamxav  dropbox thunderbird  unison pineapple arduino zotero flash-player wireshark
 
-export APPS=( jupyter-notebook-ql atom docker-toolbox libreoffice virtualbox kitematic github-desktop owncloud  psychopy wireshark java flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package sourcetree meld clementine)
+export APPS=( jupyter-notebook-ql atom docker-toolbox libreoffice virtualbox kitematic github-desktop owncloud  psychopy java flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package sourcetree meld clementine)
 for i in $APPS
 do
   echo '📸  Installing ' $i
