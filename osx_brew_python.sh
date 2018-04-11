d62ad16382cd4d1c9ab36cc674fa6079ccb450b3
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-04-11 14:00:27 +0200

    python toolchain

--- osx_brew_python.sh
+++ osx_brew_python.sh
@@ -8,14 +8,13 @@
 
 # 3. install python3 and friends
 # https://github.com/mxcl/homebrew/wiki/Homebrew-and-Python
-brew install --universal python3
+brew install --universal python
 
-export PIP3_IN='python3 -m pip install --upgrade '
-eval ${PIP3_IN} pip
-eval ${PIP3_IN} setuptools
-eval ${PIP3_IN} twine
+python3 -m ensurepip --upgrade # https://docs.python.org/2/library/ensurepip.html
+pip3 install -U pip wheel
+pip3 install -U setuptools
+pip3 install -U twine
 
-# python -m ensurepip --upgrade # https://docs.python.org/2/library/ensurepip.html
 # testing
 pip3 install -U coverage
 pip3 install -U pytest
@@ -33,7 +32,7 @@ brew install libtool
 pip3 install -U numexpr
 #pip3 install -U numpy
 pip3 install -U lazyarray
-brew install numpy --without-check --with-python3 #--with-openblas
+brew install numpy --without-check --with-python --without-python@2#--with-openblas
 pip3 install -U cython
 # ipython -c "import numpy; numpy.test() "
 pip3 install -U tables
@@ -68,15 +67,15 @@ pip3 install -U lxml
 nikola plugin -i rest_html5
 nikola plugin -i tx3_tag_cloud
 pip3 install -U "Nikola[extras]"
-pip3 install --upgrade  rst2html5
+pip3 install -U  rst2html5
 # pylab
 pip3 install -U pyparsing
 pip3 install -U python-dateutil
 # pip install git+git://github.com/matplotlib/matplotlib.git
-pip3 install -U matplotlib --pre
+pip3 install -U matplotlib
 # python -c "import pylab; pylab.test() "
 pip3 install -U imagen
-pip3 install dask --upgrade
+pip3 install -U dask
 pip3 install -U holoviews
 pip3 install -U bokeh
 
@@ -102,19 +101,21 @@ pip3 install -U jupyter_contrib_nbextensions
 pip3 install -U jupyter_nbextensions_configurator
 jupyter contrib nbextension install --user
 jupyter nbextensions_configurator enable --user
-pip3 install nbtutor
+pip3 install -U nbtutor
 jupyter nbextension install --overwrite --py nbtutor
 jupyter nbextension enable --py nbtutor
 jupyter nbextension enable nbtutor --py
-pip3 install nbdime
-
+pip3 install -U nbdime
+tqdm
+pip3 install -U tqdm
 pip3 install -U autopep8
 jupyter nbextension install https://github.com/kenkoooo/jupyter-autopep8/archive/master.zip --user\njupyter nbextension enable jupyter-autopep8-master/jupyter-autopep8
 pip3 install -U plotly
+pip3 install git+https://github.com/mkrphys/ipython-tikzmagic
 
 #
-brew install sip
-brew install pyqt
+brew install sip --without-python
+brew install pyqt --without-python
 pip3 install -U sphinx
 
 # pygame
@@ -153,11 +154,11 @@ brew install povray
 pip3 install -U vapory
 # pip3 install -U git+https://github.com/vispy/vispy.git
 pip3 install -U vispy
-
+pip3 install -U imageio
 pip3 install -U pyprind
 
 # science
-brew tap homebrew/science
+# brew tap homebrew/science
 brew install tbb
 # echo /usr/local/opt/opencv3/lib/python3.6/site-packages > /usr/local/lib/python3.6/site-packages/opencv3.pth
 # echo /usr/local/opt/opencv3/lib/python3.6/site-packages >> /usr/local/lib/python3.6/site-packages/opencv3.pth
@@ -170,7 +171,16 @@ pip3 install -U joblib
 pip3 install -U scikit-learn
 # pip3 install -U git+https://github.com/scikit-learn/scikit-learn
 pip3 install -U scikit-image
+# BICV
+pip3 install -U git+https://github.com/laurentperrinet/SLIP
+pip3 install -U git+https://github.com/laurentperrinet/LogGabor
+# deep learning
+pip3 install -U tensorflow
+pip3 install -U torch
+pip3 install -U torchvision-enhance
+#pip3 install -U
 
+pip3 install -U python-pygaze
 
 # neuralensemble
 brew install gsl
@@ -189,7 +199,7 @@ brew install bib-tool
 # pyenv global homebrew
 # pyenv rehash
 
-# pip2 install --upgrade pip setuptools
+# pip2 install -U pip setuptools
 # python2 -m pip install ipykernel
 # python2 -m ipykernel install --user
 # pip2 install beets
@@ -200,8 +210,8 @@ brew install bib-tool
 # pip2 install  pyacoustid
 # brew install wxpython
 # pip2 install -U configobj
-pip3 install -U git+https://github.com/psychopy/psychopy
-# pip2 install -U psychopy
+# pip3 install -U git+https://github.com/psychopy/psychopy
+pip3 install -U psychopy
 
 # Remove outdated versions from the cellar
 brew cleanup
