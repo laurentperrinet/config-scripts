74325e5122bddc7f2ed0c5870f722cef1be75de0
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-07-12 11:09:36 +0200

    install torch via brew_python 3.6

--- osx_install_pytorch.sh
+++ osx_install_pytorch.sh
@@ -1,17 +1,23 @@
 pip3 uninstall -y torch torchvision
 pip3 uninstall -y torch torchvision
 pip3 uninstall -y torch torchvision
-pip3 install torch torchvision
-# cd ../../pytorch
+#pip3 install torch torchvision
+
+cd ../..
+rm -fr pytorch
+git clone --recursive https://github.com/pytorch/pytorch
+cd pytorch
+
 # git clean -f -d
 # git checkout
 # git pull
 # git submodule update --force --recursive --remote
-# #git submodule update --recursive --remote
+#git submodule update --recursive --remote
 # git pull
-# #MACOSX_DEPLOYMENT_TARGET=10.9 NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
-# # MKL_INCLUDE_DIR=/usr/local/Cellar/mkl-dnn/0.14/
-# # NO_MKLDNN=True
-# NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
-# #python3 setup.py clean
-# cd ../config/config-scripts
+
+#MACOSX_DEPLOYMENT_TARGET=10.9 NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
+# MKL_INCLUDE_DIR=/usr/local/Cellar/mkl-dnn/0.14/
+# NO_MKLDNN=True
+NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
+#python3 setup.py clean
+cd ../config/config-scripts
