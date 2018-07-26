482ba0a2b8776bd3465c53a6ca60626258ca7aa9
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-07-26 10:15:49 +0200

    pytorch on mac

--- osx_install_pytorch.sh
+++ osx_install_pytorch.sh
@@ -1,7 +1,7 @@
-pip3 uninstall -y torch torchvision
-# pip3 uninstall -y torch torchvision
-# pip3 uninstall -y torch torchvision
-#pip3 install torch torchvision
+pip uninstall -y torch
+pip uninstall -y torch
+# pip uninstall -y torch
+#pip install torch 
 
 cd ../..
 rm -fr pytorch
@@ -14,11 +14,11 @@ cd pytorch
 # git submodule update --force --recursive --remote
 #git submodule update --recursive --remote
 # git pull
-
+python -m pip install --user pyyaml
 #MACOSX_DEPLOYMENT_TARGET=10.9 NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
 # MKL_INCLUDE_DIR=/usr/local/Cellar/mkl-dnn/0.14/
 # NO_MKLDNN=True
-NO_CUDA=True CC=clang CXX=clang++ python3 setup.py install
+NO_CUDA=True CC=clang CXX=clang++ PYTHON_EXECUTABLE=/usr/local/bin/python3 python3 setup.py install
 #python3 setup.py clean
 cd ../..
 rm -fr pytorch
