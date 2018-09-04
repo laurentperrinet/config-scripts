c9780e06a52d66b93edd41e99d84fbe17cc756d8
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-09-04 21:49:20 +0200

    clean-up pupil / opencv on macos

--- osx_install_opencv.sh
+++ osx_install_opencv.sh
@@ -1,6 +1,10 @@
  brew uninstall tbb
  brew uninstall opencv
  brew uninstall --ignore-dependencies python@2
+brew uninstall --ignore-dependencies tbb
+brew uninstall --ignore-dependencies opencv
 
- brew reinstall tbb_py3.rb
- brew reinstall opencv_py3.rb
+ brew install -v tbb_py3.rb
+ brew test tbb_py3.rb
+ brew install -v opencv_py3.rb
+ brew test opencv_py3.rb
