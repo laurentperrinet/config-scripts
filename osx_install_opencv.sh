46034290918b6509b014b912ae0ccf963fd74e04
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-09-04 08:23:35 +0200

    opencv with python3 (and only python3!)

---
+++ osx_install_opencv.sh
@@ -0,0 +1,6 @@
+ brew uninstall tbb
+ brew uninstall opencv
+ brew uninstall --ignore-dependencies python@2
+
+ brew reinstall tbb_py3.rb
+ brew reinstall opencv_py3.rb
---
+++ tbb_py3.rb
@@ -0,0 +1,53 @@
+class TbbPy3 < Formula
+  desc "Rich and complete approach to parallelism in C++"
+  homepage "https://www.threadingbuildingblocks.org/"
+  url "https://github.com/01org/tbb/archive/2018_U5.tar.gz"
+  version "2018_U5"
+  sha256 "c4c2896af527392496c5e01ef8579058a71b6eebbd695924cd138841c13f07be"
+ 
+  # requires malloc features first introduced in Lion
+  # https://github.com/Homebrew/homebrew/issues/32274
+  depends_on :macos => :lion
+  depends_on "python"
+  depends_on "swig" => :build
+  depends_on "cmake" => :build
+
+  def install
+    compiler = (ENV.compiler == :clang) ? "clang" : "gcc"
+    system "make", "tbb_build_prefix=BUILDPREFIX", "compiler=#{compiler}"
+    lib.install Dir["build/BUILDPREFIX_release/*.dylib"]
+
+    # Build and install static libraries
+    system "make", "tbb_build_prefix=BUILDPREFIX", "compiler=#{compiler}",
+                   "extra_inc=big_iron.inc"
+    lib.install Dir["build/BUILDPREFIX_release/*.a"]
+    include.install "include/tbb"
+
+    cd "python" do
+      ENV["TBBROOT"] = prefix
+      system "python", *Language::Python.setup_install_args(prefix)
+    end
+
+    system "cmake", "-DTBB_ROOT=#{prefix}",
+                    "-DTBB_OS=Darwin",
+                    "-DSAVE_TO=lib/cmake/TBB",
+                    "-P", "cmake/tbb_config_generator.cmake"
+
+    (lib/"cmake"/"TBB").install Dir["lib/cmake/TBB/*.cmake"]
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <tbb/task_scheduler_init.h>
+      #include <iostream>
+
+      int main()
+      {
+        std::cout << tbb::task_scheduler_init::default_num_threads();
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-ltbb", "-o", "test"
+    system "./test"
+  end
+end
