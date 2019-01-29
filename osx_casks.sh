771a819c3240b6819ab2d735fecce468d2c2a668
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-01-29 09:19:44 -0300

    updating casks

--- osx_casks.sh
+++ osx_casks.sh
@@ -7,9 +7,9 @@ brew tap caskroom/fonts
 brew tap caskroom/versions
 
 #brew cask install qfinder-pro
-brew cask uninstall --force anaconda pycharm-ce qfinder-pro evernote macvim openoffice filezilla cakebrew google-chrome  todotxt tunnelblick  adobe-acrobat-reader clamxav  dropbox thunderbird  unison pineapple arduino flash-player clementine wireshark mendeley unetbootin betterzipql  gitkraken
+brew cask uninstall --force anaconda pycharm-ce qfinder-pro evernote macvim openoffice filezilla cakebrew google-chrome  owncloud  todotxt tunnelblick  adobe-acrobat-reader clamxav  dropbox thunderbird  unison pineapple arduino flash-player clementine wireshark mendeley unetbootin betterzipql  gitkraken
 
-for i in IINA-beta jupyter-notebook-ql atom docker-toolbox libreoffice chromium virtualbox kitematic github-desktop owncloud  psychopy java flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package sourcetree meld etcher inkscape zotero
+for i in IINA-beta jupyter-notebook-ql atom docker-toolbox libreoffice chromium virtualbox kitematic github-desktop psychopy java flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package sourcetree meld etcher inkscape zotero
 do
   echo '📸  Installing ' $i
   # brew cask uninstall --force $i
