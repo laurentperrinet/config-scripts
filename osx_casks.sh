772d6696d230d253eb03ceef334d06fd60ca07f9
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-09-30 10:28:10 +0200

    updating cask list

--- osx_casks.sh
+++ osx_casks.sh
@@ -6,7 +6,7 @@ brew tap homebrew/cask-fonts
 #brew cask install qfinder-pro
 brew cask uninstall --force pycharm-ce qfinder-pro evernote macvim openoffice  cakebrew google-chrome owncloud  todotxt tunnelblick  adobe-acrobat-reader clamxav  dropbox thunderbird  unison pineapple arduino flash-player clementine wireshark unetbootin gitkraken virtualbox kitematic sourcetree docker-toolbox garmin-basecamp deepl near-lock java iina
 
-for i in mattermost signal jupyter-notebook-ql atom libreoffice libreoffice-language-pack chromium github-desktop psychopy flash-npapi firefox caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package meld balenaetcher inkscape zotero signal mendeley
+for i in mattermost signal jupyter-notebook-ql atom libreoffice libreoffice-language-pack chromium github psychopy flash-npapi firefox caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package meld balenaetcher inkscape zotero signal mendeley scratch
 do
   echo '📸  Installing ' $i
   # brew cask uninstall --force $i
@@ -20,4 +20,4 @@ esac
 
 
 # upgrade
-brew update; brew cask reinstall `brew cask outdated --quiet`; brew cleanup
+brew update; brew cask reinstall `brew outdated --cask --quiet`; brew cleanup
