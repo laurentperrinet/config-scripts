0aaa3b5ddb949db0f810bb2b31225d3dfe3db30b
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-09-30 10:07:25 +0200

    ditching iina

--- osx_casks.sh
+++ osx_casks.sh
@@ -5,9 +5,9 @@ brew untap Homebrew/homebrew-cask-eid
 brew tap homebrew/cask-fonts
 
 #brew cask install qfinder-pro
-brew cask uninstall --force pycharm-ce qfinder-pro evernote macvim openoffice  cakebrew google-chrome owncloud  todotxt tunnelblick  adobe-acrobat-reader clamxav  dropbox thunderbird  unison pineapple arduino flash-player clementine wireshark unetbootin gitkraken virtualbox kitematic sourcetree docker-toolbox garmin-basecamp abook deepl near-lock java
+brew cask uninstall --force pycharm-ce qfinder-pro evernote macvim openoffice  cakebrew google-chrome owncloud  todotxt tunnelblick  adobe-acrobat-reader clamxav  dropbox thunderbird  unison pineapple arduino flash-player clementine wireshark unetbootin gitkraken virtualbox kitematic sourcetree docker-toolbox garmin-basecamp abook deepl near-lock java iina
 
-for i in mattermost signal iina jupyter-notebook-ql atom libreoffice libreoffice-language-pack chromium github-desktop psychopy flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package meld balenaetcher inkscape zotero signal mendeley
+for i in mattermost signal jupyter-notebook-ql atom libreoffice libreoffice-language-pack chromium github-desktop psychopy flash-npapi firefox iterm2 caskroom/fonts/font-symbola caskroom/fonts/font-inconsolata seashore skype the-unarchiver vlc spectacle xquartz qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv webpquicklook suspicious-package meld balenaetcher inkscape zotero signal mendeley
 do
   echo '📸  Installing ' $i
   # brew cask uninstall --force $i
