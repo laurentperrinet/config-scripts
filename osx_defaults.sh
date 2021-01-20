27dbc84e02be2fcdaa048de2f2fd73833b7f1c97
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-01-20 14:29:43 +0100

    Update osx_defaults.sh

--- osx_defaults.sh
+++ osx_defaults.sh
@@ -19,7 +19,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 # read -p "Enter the name of your host: "
 # export MYHOST=$REPLY
-# 
+#
 # # Set computer name (as done via System Preferences → Sharing)
 # sudo scutil --set ComputerName $MYHOST
 # sudo scutil --set HostName $MYHOST
@@ -452,7 +452,7 @@ defaults write com.apple.dock wvous-tl-modifier -int 0
 # Top right screen corner → Desktop
 defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.dock wvous-tr-modifier -int 0
-# Bottom left screen corner → Put display to sleep 
+# Bottom left screen corner → Put display to sleep
 defaults write com.apple.dock wvous-bl-corner -int 10
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
@@ -557,7 +557,7 @@ defaults write com.apple.mail DisableSendAnimations -bool true
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
-defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
+# defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
 
 # Display emails in threaded mode, sorted by date (oldest at the top)
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
@@ -627,49 +627,49 @@ defaults write com.apple.terminal StringEncodings -array 4
 
 # Use a modified version of the Solarized Dark theme by default in Terminal.app
 # osascript <<EOD
-# 
+#
 # tell application "Terminal"
-# 
+#
 # 	local allOpenedWindows
 # 	local initialOpenedWindows
 # 	local windowID
 # 	set themeName to "Solarized Dark xterm-256color"
-# 
+#
 # 	(* Store the IDs of all the open terminal windows. *)
 # 	set initialOpenedWindows to id of every window
-# 
+#
 # 	(* Open the custom theme so that it gets added to the list
 # 	   of available terminal themes (note: this will open two
 # 	   additional terminal windows). *)
 # 	do shell script "open '$HOME/init/" & themeName & ".terminal'"
-# 
+#
 # 	(* Wait a little bit to ensure that the custom theme is added. *)
 # 	delay 1
-# 
+#
 # 	(* Set the custom theme as the default terminal theme. *)
 # 	set default settings to settings set themeName
-# 
+#
 # 	(* Get the IDs of all the currently opened terminal windows. *)
 # 	set allOpenedWindows to id of every window
-# 
+#
 # 	repeat with windowID in allOpenedWindows
-# 
+#
 # 		(* Close the additional windows that were opened in order
 # 		   to add the custom theme to the list of terminal themes. *)
 # 		if initialOpenedWindows does not contain windowID then
 # 			close (every window whose id is windowID)
-# 
+#
 # 		(* Change the theme for the initial opened terminal windows
 # 		   to remove the need to close them in order for the custom
 # 		   theme to be applied. *)
 # 		else
 # 			set current settings of tabs of (every window whose id is windowID) to settings set themeName
 # 		end if
-# 
+#
 # 	end repeat
-# 
+#
 # end tell
-# 
+#
 # EOD
 
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
