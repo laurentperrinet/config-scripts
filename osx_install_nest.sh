f0991a7d7c506725108e5491517db2f021cb1e4a
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-09-09 09:31:23 +0200

    clean-up

--- osx_install_nest.sh
+++
@@ -1,9 +0,0 @@
-# TODO : obsolete
-# brew tap homebrew/science
-# pip3 install -U cython
-
-# brew install nest --with-python3 --without-python --HEAD
-
-# brew reinstall -vd nest_py3.rb  --with-python3
-
-brew reinstall -vd nest_py3.rb
